946c70686f083371ca5ac74e209771f48cc6ac44
= <=>
2024-11-22 15:54:49 -0600

    stuff

---
+++ snakeTower.py
@@ -0,0 +1,25 @@
+#snakeTower
+
+import pygame as pg
+
+pg.init()
+screen = pg.display.set_mode((1920, 1080))
+pg.display.set_caption("snakeTower")
+clock = pg.time.Clock()
+
+running = True
+while running:
+    # Handle events
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+    
+    # Game logic here
+    
+    ""# Draw everything
+    screen.fill((0, 0, 0))  # Fill screen with black
+    pg.display.flip()    # Update the display
+    
+    clock.tick(60)  # Limit to 60 FPS""
+
+pg.quit()
